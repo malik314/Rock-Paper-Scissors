cd618cf7076e5164f2b90a9c81293065b30f2647
malik314 <74277785+malik314@users.noreply.github.com>
2022-08-23 17:46:56 -0500

    update

--- home.js
+++ home.js
@@ -1,2 +1,41 @@
+function getComputerChoice() {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    const rand = Math.floor(Math.random() * options.length);
+    return options[rand];
+}
 
+function getPlayerChoice(options) {
+    options = prompt("Enter a choice: Rock, Paper, or Scissors");
+    return options;
+}
 
+function round(playerSelection, computerSelection) {
+    console.log('Player Choice: ', playerSelection);
+    console.log('Computer Choice: ', computerSelection);
+
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        console.log('Computer Wins!');
+    } 
+    else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        console.log('Player Wins!');
+    }
+    else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        console.log('Player Wins!');
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        console.log('Computer Wins!');
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        console.log('Computer Wins!');
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        console.log('Player Wins!');
+    }
+    else if (playerSelection === computerSelection ) {
+        console.log('Tie!');
+    }
+}
+
+let player = getPlayerChoice();
+let ai = getComputerChoice();
+round(player, ai);
